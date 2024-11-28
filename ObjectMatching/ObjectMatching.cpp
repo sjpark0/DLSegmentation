aeb1ec29f6107b3e2f09d162fa768004ae379d8d
sjpark0 <sjpark0@gmail.com>
2024-11-28 21:33:48 +0900

    object matching추가

--- ObjectMatching/ObjectMatching.cpp
+++ ObjectMatching/ObjectMatching.cpp
@@ -2,185 +2,49 @@
 //
 
 #include <iostream>
-void ComputeInitialResult(const unsigned char* image, ImgDimension dims, int numChannelImage, unsigned char* object, ImgDimension objDims, int numChannelObject, int* startPixel, int* endPixel, int refCamID, const float* matRotation, const float* matTranslation, const float* matIntrinsic)
-{	
-	m_pImage = image;
-	m_pObject = object;
-	m_pDim = dims;
-	m_pObjDim = objDims;
+#include <opencv2/opencv.hpp>
+using namespace cv;
+void ChangeOneStepOffset(int width, int height, float* refC2W, float* fltW2C, float refFocal, float fltFocal, float zValue, float& offsetX, float& offsetY, int pointX, int pointY)
+{
+    float origin[4];
+    float dir[4];
+    float t_origin[4];
+    float t_dir[4];
+    float trans[3];
+    float tr;
+    int ix = pointX;
+    int iy = pointY;
+    int newX, newY;
+    int centerX = width / 2;
+    int centerY = height / 2;
+    origin[0] = refC2W[12];
+    origin[1] = refC2W[13];
+    origin[2] = refC2W[14];
+    origin[3] = refC2W[15];
 
-	m_pStartPixel = startPixel;
-	m_pEndPixel = endPixel;
-	m_iRefCamID = refCamID;
-	//m_pRotation = matRotation;
-	//m_pTranslation = matTranslation;
-	//m_pIntrinsic = matIntrinsic;
-	m_numChannelImage = numChannelImage;
-	m_numChannelObject = numChannelObject;
+    dir[0] = refC2W[0] * ((ix - centerX) / refFocal) - refC2W[4] * ((iy - centerY) / refFocal) - refC2W[8];
+    dir[1] = refC2W[1] * ((ix - centerX) / refFocal) - refC2W[5] * ((iy - centerY) / refFocal) - refC2W[9];
+    dir[2] = refC2W[2] * ((ix - centerX) / refFocal) - refC2W[6] * ((iy - centerY) / refFocal) - refC2W[10];
+    dir[3] = refC2W[3] * ((ix - centerX) / refFocal) - refC2W[7] * ((iy - centerY) / refFocal) - refC2W[11];
 
+    t_origin[0] = fltW2C[0] * origin[0] + fltW2C[4] * origin[1] + fltW2C[8] * origin[2] + fltW2C[12] * origin[3];
+    t_origin[1] = fltW2C[1] * origin[0] + fltW2C[5] * origin[1] + fltW2C[9] * origin[2] + fltW2C[13] * origin[3];
+    t_origin[2] = fltW2C[2] * origin[0] + fltW2C[6] * origin[1] + fltW2C[10] * origin[2] + fltW2C[14] * origin[3];
+    t_origin[3] = fltW2C[3] * origin[0] + fltW2C[7] * origin[1] + fltW2C[11] * origin[2] + fltW2C[15] * origin[3];
 
-	m_ptTranslation = new Point2f[m_pDim.numCam];
-	for (int i = 0; i < m_pDim.numCam; i++) {
-		m_ptTranslation[i].x = m_ptTranslation[i].y = 0;
-		m_pStartPixel[i * 2] = m_pStartPixel[m_iRefCamID * 2];
-		m_pStartPixel[i * 2 + 1] = m_pStartPixel[m_iRefCamID * 2 + 1];
-		m_pEndPixel[i * 2] = m_pEndPixel[m_iRefCamID * 2];
-		m_pEndPixel[i * 2 + 1] = m_pEndPixel[m_iRefCamID * 2 + 1];
-	}
-	int index1;
-	int index2;
-	// 뎁스계산 코드
-	int cid = m_iRefCamID + 1;
+    t_dir[0] = fltW2C[0] * dir[0] + fltW2C[4] * dir[1] + fltW2C[8] * dir[2] + fltW2C[12] * dir[3];
+    t_dir[1] = fltW2C[1] * dir[0] + fltW2C[5] * dir[1] + fltW2C[9] * dir[2] + fltW2C[13] * dir[3];
+    t_dir[2] = fltW2C[2] * dir[0] + fltW2C[6] * dir[1] + fltW2C[10] * dir[2] + fltW2C[14] * dir[3];
+    t_dir[3] = fltW2C[3] * dir[0] + fltW2C[7] * dir[1] + fltW2C[11] * dir[2] + fltW2C[15] * dir[3];
 
-	const float* pIntrinsicDst = &matIntrinsic[m_iRefCamID * 4];
+    tr = (-zValue - t_origin[2]) / t_dir[2];
 
-	const float* pIntrinsicSrc = &matIntrinsic[cid * 4];
-	const float* pRotation = &matRotation[cid * 9];
-	const float* pTranslation = &matTranslation[cid * 3];
+    trans[0] = t_origin[0] + tr * t_dir[0];
+    trans[1] = t_origin[1] + tr * t_dir[1];
+    trans[2] = t_origin[2] + tr * t_dir[2];
 
-	float fDirX, fDirY;
-	float fTxOrig3DX, fTxOrig3DY, fTxOrig3DZ;
-	float xTx, yTx, zTx;
-	int x, y;
-	int cnt;
-	float sum;
-	float zValue;
-	float optSum;
-	int optD;
-
-	for (int d = 0; d < 100; d += 10) {
-		cnt = 0;
-		sum = 0;
-		zValue = d * 0.007 / 100.0;
-		for (int i = m_pStartPixel[m_iRefCamID * 2 + 1]; i <= m_pEndPixel[m_iRefCamID * 2 + 1]; i++) {
-			for (int j = m_pStartPixel[m_iRefCamID * 2]; j <= m_pEndPixel[m_iRefCamID * 2]; j++) {
-				fDirX = ((float)j - pIntrinsicDst[2]) / pIntrinsicDst[0];
-				fDirY = ((float)i - pIntrinsicDst[3]) / pIntrinsicDst[1];
-				fTxOrig3DX = pRotation[0] * fDirX + pRotation[1] * fDirY + pRotation[2];
-				fTxOrig3DY = pRotation[3] * fDirX + pRotation[4] * fDirY + pRotation[5];
-				fTxOrig3DZ = pRotation[6] * fDirX + pRotation[7] * fDirY + pRotation[8];
-
-				xTx = fTxOrig3DX + zValue * pTranslation[0];
-				yTx = fTxOrig3DY + zValue * pTranslation[1];
-				zTx = fTxOrig3DZ + zValue * pTranslation[2];
-
-				x = (int)((xTx / zTx * pIntrinsicSrc[0]) + pIntrinsicSrc[2] + 0.5f);
-				y = (int)((yTx / zTx * pIntrinsicSrc[1]) + pIntrinsicSrc[3] + 0.5f);
-				if (x >= 0 && x < (int)m_pDim.width && y >= 0 && y < (int)m_pDim.height) {
-					index1 = j + i * m_pDim.width + m_iRefCamID * m_pDim.height * m_pDim.width;
-					index2 = x + y * m_pDim.width + cid * m_pDim.height * m_pDim.width;
-					sum += abs(m_pImage[index1 * numChannelImage] - m_pImage[index2 * numChannelImage]) + abs(m_pImage[index1 * numChannelImage + 1] - m_pImage[index2 * numChannelImage + 1]) + abs(m_pImage[index1 * numChannelImage + 2] - m_pImage[index2 * numChannelImage + 2]);
-					cnt++;
-				}
-			}
-		}
-		sum = sum / cnt;
-		if (d == 0) {
-			optSum = sum;
-			optD = d;
-		}
-		else {
-			if (optSum > sum) {
-				optSum = sum;
-				optD = d;
-			}
-		}
-	}
-	int startD = optD - 10;
-	int endD = optD + 10;
-	for (int d = startD; d <= endD; d++) {
-		cnt = 0;
-		sum = 0;
-		zValue = d * 0.007 / 100.0;
-		for (int i = m_pStartPixel[m_iRefCamID * 2 + 1]; i <= m_pEndPixel[m_iRefCamID * 2 + 1]; i++) {
-			for (int j = m_pStartPixel[m_iRefCamID * 2]; j <= m_pEndPixel[m_iRefCamID * 2]; j++) {
-				fDirX = ((float)j - pIntrinsicDst[2]) / pIntrinsicDst[0];
-				fDirY = ((float)i - pIntrinsicDst[3]) / pIntrinsicDst[1];
-				fTxOrig3DX = pRotation[0] * fDirX + pRotation[1] * fDirY + pRotation[2];
-				fTxOrig3DY = pRotation[3] * fDirX + pRotation[4] * fDirY + pRotation[5];
-				fTxOrig3DZ = pRotation[6] * fDirX + pRotation[7] * fDirY + pRotation[8];
-
-				xTx = fTxOrig3DX + zValue * pTranslation[0];
-				yTx = fTxOrig3DY + zValue * pTranslation[1];
-				zTx = fTxOrig3DZ + zValue * pTranslation[2];
-
-				x = (int)((xTx / zTx * pIntrinsicSrc[0]) + pIntrinsicSrc[2] + 0.5f);
-				y = (int)((yTx / zTx * pIntrinsicSrc[1]) + pIntrinsicSrc[3] + 0.5f);
-				if (x >= 0 && x < (int)m_pDim.width && y >= 0 && y < (int)m_pDim.height) {
-					index1 = j + i * m_pDim.width + m_iRefCamID * m_pDim.height * m_pDim.width;
-					index2 = x + y * m_pDim.width + cid * m_pDim.height * m_pDim.width;
-					sum += abs(m_pImage[index1 * numChannelImage] - m_pImage[index2 * numChannelImage]) + abs(m_pImage[index1 * numChannelImage + 1] - m_pImage[index2 * numChannelImage + 1]) + abs(m_pImage[index1 * numChannelImage + 2] - m_pImage[index2 * numChannelImage + 2]);
-					cnt++;
-				}
-			}
-		}
-		sum = sum / cnt;
-		//printf("%d, %d, %f\n", d, cnt, sum);
-		if (d == 0) {
-			optSum = sum;
-			optD = d;
-		}
-		else {
-			if (optSum > sum) {
-				optSum = sum;
-				optD = d;
-			}
-		}
-	}
-	//optD = 28;
-	zValue = optD * 0.007 / 100.0;
-	for (int i = 0; i < m_pDim.numCam; i++) {
-		if (i != m_iRefCamID) {
-			pIntrinsicSrc = &matIntrinsic[i * 4];
-			pRotation = &matRotation[i * 9];
-			pTranslation = &matTranslation[i * 3];
-
-			fDirX = ((float)m_pStartPixel[m_iRefCamID * 2] - pIntrinsicDst[2]) / pIntrinsicDst[0];
-			fDirY = ((float)m_pStartPixel[m_iRefCamID * 2 + 1] - pIntrinsicDst[3]) / pIntrinsicDst[1];
-			fTxOrig3DX = pRotation[0] * fDirX + pRotation[1] * fDirY + pRotation[2];
-			fTxOrig3DY = pRotation[3] * fDirX + pRotation[4] * fDirY + pRotation[5];
-			fTxOrig3DZ = pRotation[6] * fDirX + pRotation[7] * fDirY + pRotation[8];
-
-			xTx = fTxOrig3DX + zValue * pTranslation[0];
-			yTx = fTxOrig3DY + zValue * pTranslation[1];
-			zTx = fTxOrig3DZ + zValue * pTranslation[2];
-
-			m_pStartPixel[i * 2] = MAX(0, (int)((xTx / zTx * pIntrinsicSrc[0]) + pIntrinsicSrc[2] + 0.5f));
-			m_pStartPixel[i * 2 + 1] = MAX(0, (int)((yTx / zTx * pIntrinsicSrc[1]) + pIntrinsicSrc[3] + 0.5f));
-
-			m_pEndPixel[i * 2] = MIN(m_pDim.width - 1, m_pStartPixel[i * 2] + m_pObjDim.width - 1);
-			m_pEndPixel[i * 2 + 1] = MIN(m_pDim.height - 1, m_pStartPixel[i * 2 + 1] + m_pObjDim.height - 1);
-			/*fDirX = ((float)m_pEndPixel[m_iRefCamID * 2] - pIntrinsicDst[2]) / pIntrinsicDst[0];
-			fDirY = ((float)m_pEndPixel[m_iRefCamID * 2 + 1] - pIntrinsicDst[3]) / pIntrinsicDst[1];
-			fTxOrig3DX = pRotation[0] * fDirX + pRotation[1] * fDirY + pRotation[2];
-			fTxOrig3DY = pRotation[3] * fDirX + pRotation[4] * fDirY + pRotation[5];
-			fTxOrig3DZ = pRotation[6] * fDirX + pRotation[7] * fDirY + pRotation[8];
-
-			xTx = fTxOrig3DX + zValue * pTranslation[0];
-			yTx = fTxOrig3DY + zValue * pTranslation[1];
-			zTx = fTxOrig3DZ + zValue * pTranslation[2];
-
-			m_pEndPixel[i * 2] = MIN(m_pDim.width - 1, (int)((xTx / zTx * pIntrinsicSrc[0]) + pIntrinsicSrc[2] + 0.5f));
-			m_pEndPixel[i * 2 + 1] = MIN(m_pDim.height - 1, (int)((yTx / zTx * pIntrinsicSrc[1]) + pIntrinsicSrc[3] + 0.5f));*/
-		}
-	}
-
-	//
-
-
-	/*for (int i = 0; i < m_pDim[0] * m_pDim[1]; i++) {
-		for (int j = m_pStartPixel[i * 2 + 1]; j < m_pStartPixel[i * 2 + 1] + m_pObjDim[2]; j++) {
-			for (int k = m_pStartPixel[i * 2]; k < m_pStartPixel[i * 2] + m_pObjDim[3]; k++) {
-				index1 = k + j * m_pDim[3] + i * m_pDim[2] * m_pDim[3];
-				index2 = (k - m_pStartPixel[i * 2]) + (j - m_pStartPixel[i * 2 + 1]) * m_pObjDim[3] + i * m_pObjDim[2] * m_pObjDim[3];
-
-				for (int l = 0; l < m_numChannelImage - 1; l++) {
-					m_pObject[index2 * m_numChannelObject + l] = m_pImage[index1 * m_numChannelImage + l];
-				}
-			}
-		}
-	}*/
-
+    offsetX = trans[0] / -trans[2] * fltFocal + centerX - ix;
+    offsetY = -(trans[1] / -trans[2] * fltFocal) + centerY - iy;
 }
 
 void MPILoader(const char* foldername, float* c2w, float* w2c, float* cif, int numCam)
@@ -228,15 +92,181 @@ void MPILoader(const char* foldername, float* c2w, float* w2c, float* cif, int n
     }
     
 }
+void ComputeOffset(unsigned char* image, int width, int height, int numCam, int* boundingBox, float* c2w, float* w2c, float* cif, int refCamID, float* offsetX, float* offsetY)
+{
+    float zValueCurrent;
+    float zstep = ((1.0 / cif[refCamID * 3]) - (1.0 / cif[1 + refCamID * 3]));
+    int cnt;
+    int newX;
+    int newY;
+    float tmpMean;
+    float mean;
+    
+    float optMean;
+    float optZ;
+    float val1, val2, val3;
+    int numAvailableCam;
+    optZ = -1.0;
+    optMean = 10000000000;
+    for (float zValue = 0.0; zValue < 10.0; zValue += 0.1) {
+        
+        zValueCurrent = 1.0 / (zstep * zValue + 1.0 / cif[1 + refCamID * 3]);
+        mean = 0.0f;
+        numAvailableCam = 0;
+        for (int i = 0; i < numCam; i++) {
+            if (i == refCamID) continue;
+            ChangeOneStepOffset(width, height, &c2w[refCamID * 16], &c2w[i * 16], cif[2 + refCamID * 3], cif[2 + i * 3], zValueCurrent, offsetX[i], offsetY[i], boundingBox[0], boundingBox[1]);
+            cnt = 0;
+            tmpMean = 0.0f;
+            for (int y = boundingBox[1]; y <= boundingBox[3]; y++) {
+                for (int x = boundingBox[0]; x <= boundingBox[2]; x++) {
+                    newX = (int)(x + offsetX[i] + 0.5f);
+                    newY = (int)(y + offsetY[i] + 0.5f);
+                    if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
+                        val1 = (image[(x + y * width + refCamID * width * height) * 3] - image[(newX + newY * width + i * width * height) * 3]);
+                        val2 = (image[(x + y * width + refCamID * width * height) * 3 + 1] - image[(newX + newY * width + i * width * height) * 3 + 1]);
+                        val3 = (image[(x + y * width + refCamID * width * height) * 3 + 2] - image[(newX + newY * width + i * width * height) * 3 + 2]);
+                        tmpMean += (val1 * val1) + (val2 * val2) + (val3 * val3);
+                        cnt++;
+                    }
+                }
+            }
+            if (cnt > 0) {
+                mean += tmpMean / cnt;
+                numAvailableCam++;
+            }
+        }
+        printf("%f, %f\n", zValueCurrent, mean);
+        if (numAvailableCam > 0) {
+            if (optMean > (mean / numAvailableCam)) {
+                optMean = mean / numAvailableCam;
+                optZ = zValue;
+            }
+        }
+    }
+    printf("%f\n", optZ);
+    for (int i = 0; i < numCam; i++) {
+        ChangeOneStepOffset(width, height, &c2w[refCamID * 16], &c2w[i * 16], cif[2 + refCamID * 3], cif[2 + i * 3], optZ, offsetX[i], offsetY[i], boundingBox[0], boundingBox[1]);
+    }
+}
 int main()
 {
     char foldername[] = "..\\Data\\Sample1";
+    char filename[1024];
+
     int numCam = 16;
     float* pC2W = new float[numCam * 16];
     float* pW2C = new float[numCam * 16];
     float* pCIF = new float[numCam * 3];
+    int boundingBox[4];
     MPILoader(foldername, pC2W, pW2C, pCIF, numCam);
+    
+    sprintf_s(filename, "%s\\images\\000.png", foldername);
+    Mat img = imread(filename);
+    int width = img.cols;
+    int height = img.rows;
+    int refCamID = 0;
+    int fltCamID = 15;
+    unsigned char* pImage = new unsigned char[width * height * numCam * 3];
     for (int c = 0; c < numCam; c++) {
+        sprintf_s(filename, "%s\\images\\%03d.png", foldername, c);
+        img = imread(filename);
+        memcpy(&pImage[c * width * height * 3], img.data, width * height * 3 * sizeof(unsigned char));
+    }
+    
+    img = Mat(height, width, CV_8UC3, &pImage[refCamID * width * height * 3]);
+    Mat origin = img.clone();
+    namedWindow("Select ROI", cv::WINDOW_NORMAL);
+    resizeWindow("Select ROI", width / 4, height / 4);
+    moveWindow("Select ROI", 0, 0);
+    waitKey(1);
+    Rect init_rect = selectROI("Select ROI", img, true, false); // ROI 박스 치기
+    waitKey(0);
+    destroyWindow("Select ROI");
+    
+    boundingBox[0] = init_rect.x;
+    boundingBox[1] = init_rect.y;
+    boundingBox[2] = init_rect.x + init_rect.width;
+    boundingBox[3] = init_rect.y + init_rect.height;
+
+    printf("%d, %d, %d, %d\n", boundingBox[0], boundingBox[1], boundingBox[2], boundingBox[3]);
+
+    float* offsetX = new float[numCam];
+    float* offsetY = new float[numCam];
+    memset(offsetX, 0, numCam * sizeof(float));
+    memset(offsetY, 0, numCam * sizeof(float));
+
+    ComputeOffset(pImage, width, height, numCam, boundingBox, pC2W, pW2C, pCIF, refCamID, offsetX, offsetY);
+    
+    namedWindow("Visualize ROI", cv::WINDOW_NORMAL);
+    resizeWindow("Visualize ROI", width / 4, height / 4);
+    moveWindow("Visualize ROI", 0, 0);
+    for (int i = 0; i < numCam; i++) {
+        img = Mat(height, width, CV_8UC3, &pImage[i * width * height * 3]);
+        printf("%f, %f\n", offsetX[i], offsetY[i]);
+        rectangle(img, Rect((int)(boundingBox[0] + offsetX[i]), (int)(boundingBox[1] + offsetY[i]), init_rect.width, init_rect.height), Scalar(255, 0, 0), 2, 8, 0);
+        imshow("Visualize ROI", img);
+        getchar();
+        waitKey(1);
+    }
+    /*img = Mat(height, width, CV_8UC3, &pImage[fltCamID * width * height * 3]);
+    origin = img.clone();
+    namedWindow("Visualize ROI", cv::WINDOW_NORMAL);
+    resizeWindow("Visualize ROI", width / 4, height / 4);
+    moveWindow("Visualize ROI", 0, 0);
+    waitKey(1);
+    float zstep = ((1.0 / pCIF[refCamID * 3]) - (1.0 / pCIF[1 + refCamID * 3]));
+    float zValueCurrent;
+    for (float zValue = 0.0; zValue < 1.0; zValue += 0.1) {
+        zValueCurrent = 1.0 / (zstep * zValue + 1.0 / pCIF[1 + refCamID * 3]);
+
+        ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX[fltCamID], offsetY[fltCamID], boundingBox[0], boundingBox[1]);
+        //ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX[fltCamID], offsetY[fltCamID], 0, 0);
+        printf("%f, %f, %f\n", zValueCurrent, offsetX[fltCamID], offsetY[fltCamID]);
+        img = origin.clone();
+        rectangle(img, Rect((int)(boundingBox[0] + offsetX[fltCamID]), (int)(boundingBox[1] + offsetY[fltCamID]), init_rect.width, init_rect.height), Scalar(255, 0, 0), 2, 8, 0);
+        imshow("Visualize ROI", img);
+        getchar();
+        waitKey(1);
+    }*/
+    /*float zValue;
+    float offsetX1, offsetX2, offsetY1, offsetY2;
+    zValue = 0.0;
+    zValueCurrent = 1.0 / (zstep * zValue + 1.0 / pCIF[1 + refCamID * 3]);
+    ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX1, offsetY1, width / 2, height / 2);
+
+    
+    zValue = 1.0;
+    zValueCurrent = 1.0 / (zstep * zValue + 1.0 / pCIF[1 + refCamID * 3]);
+    ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX2, offsetY2, width / 2, height / 2);
+
+    printf("%f, %f\n", boundingBox[0] + offsetX1, boundingBox[1] + offsetY1);
+    printf("%f, %f\n", boundingBox[0] + offsetX2, boundingBox[1] + offsetY2);
+
+    
+    zValue = 0.0;
+    zValueCurrent = 1.0 / (zstep * zValue + 1.0 / pCIF[1 + refCamID * 3]);
+    ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX1, offsetY1, 0, 0);
+
+    
+    zValue = 1.0;
+    zValueCurrent = 1.0 / (zstep * zValue + 1.0 / pCIF[1 + refCamID * 3]);
+    ChangeOneStepOffset(width, height, &pC2W[refCamID * 16], &pW2C[fltCamID * 16], pCIF[2 + refCamID * 3], pCIF[2 + fltCamID * 3], zValueCurrent, offsetX2, offsetY2, 0, 0);
+
+    printf("%f, %f\n", boundingBox[0] + offsetX1, boundingBox[1] + offsetY1);
+    printf("%f, %f\n", boundingBox[0] + offsetX2, boundingBox[1] + offsetY2);*/
+
+    //rectangle(img, Rect((int)(boundingBox[0] + offsetX[fltCamID]), (int)(boundingBox[1] + offsetY[fltCamID]), init_rect.width, init_rect.height), Scalar(255, 0, 0), 2, 8, 0);
+    //imshow("Visualize ROI", img);
+    //waitKey(0);
+    destroyWindow("Visualize ROI");
+    delete[]pC2W;
+    delete[]pW2C;
+    delete[]pCIF;
+    delete[]offsetX;
+    delete[]offsetY;
+    delete[]pImage;
+    /*for (int c = 0; c < numCam; c++) {
         printf("%dth camera\n", c);
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -254,7 +284,7 @@ int main()
             }
             printf("\n");
         }
-    }
+    }*/
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
